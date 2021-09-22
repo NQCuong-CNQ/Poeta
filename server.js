5c125f15019ce3f51ac0faf30081ae5bda87b8f3
Quoc Cuong <nqcuong.cnq@gmail.com>
2021-09-22 18:40:00 +0700

    ádf

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
 const express = require("express")
 const app = express()
-const http = require("http")
+var cors = require('cors')
+const https = require("https")
 
 var mainRoute = require('./routers/main-router')
 
+var server = https.createServer({
+  cert: fs.readFileSync("./ssl/fullchain.pem"),
+  key: fs.readFileSync("./ssl/privkey.pem"),
+}, app)
+
+
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/public/views/')
+
+app.use(cors())
+app.options('*', cors())
+
 app.use(express.static("public"))
 app.use('/', mainRoute)
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header('Access-Control-Allow-Methods', '*')
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, append,delete,entries,foreach,get,has,keys,set,values,Authorization")
-  if ('OPTIONS' == req.method) {
-    res.sendStatus(200)
-  }
-  else {
-    next()
-  }
-})
-
-app.listen(80, () => {
-  console.log('Application listening on port 3333!');
-});
+server.listen(80)
